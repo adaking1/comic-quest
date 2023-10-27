184f7fc0fb2d0782313e45c050bfaf45f2989af4
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-10-26 22:01:21 -0400

    added model definitions

---
+++ models/Comic.js
@@ -0,0 +1,31 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comic extends Model {};
+
+Comic.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comic'
+    }
+);
+
+module.exports = Comic;
