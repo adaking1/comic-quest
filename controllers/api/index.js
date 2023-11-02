0ca4efe3b8a72324cf5e8fca676524dda3b8bb7d
Piero Rodriguez <piero@Pieros-MacBook-Pro.local>
2023-11-01 20:25:38 -0400

    added routes to index.js in api folder and added new index.js to controllers folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
+router.use('/comment', commentRoutes);
+
+module.exports = router;
+
