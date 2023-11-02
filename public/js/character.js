b0ccdf6937934dc0a853a0e3e59779a7346b409b
Piero Rodriguez <piero@Pieros-MacBook-Pro.local>
2023-11-02 16:06:18 -0400

    added character route code, fixed routes, fixed chracter model

---
+++ public/js/character.js
@@ -0,0 +1,18 @@
+const searchChar = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('')
+}
+
+
+
+function  
+const querySelector
+if charName
+post
+fetch /api/character/:id
+if response ok 
+else 
+
+
+
