690e1c059136e340aa7da8a048e534cdaf91ebf4
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-11-01 20:52:44 -0400

    added home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,12 +20,27 @@ router.get('/', async (req,res) => {
 // route to render user's profile
 router.get('/profile', withAuth, async (req,res) => {
     try {
-        const userData = await User.findByPk(req.session.user_id);
-        
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: {exclude: ['password']}
+        });
+        const user = userdata.get({plain:true});
+
+        res.render('profile', {...user, loggedIn: req.session.logged_in});
     }
     catch (err) {
         res.status(500).json(err);
     }
+});
+
+// redirects user to their profile if they are already logged in
+// renders the login page if they are not logged in
+router.get('/login', (req,res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
 });
 
 
