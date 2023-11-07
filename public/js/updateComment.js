53885355458898adffbf21a717aa58fcebdfce63
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-11-07 18:01:05 -0500

    fixed update comment input

--- public/js/updateComment.js
+++ public/js/updateComment.js
@@ -1,10 +1,10 @@
 
 const updateCommentBtn = async (event) => {
     event.preventDefault();
-    console.log(event);
     const updatedCom = document.querySelector('#updateComment').value.trim();
     const id = event.target.getAttribute('data-commentId');
-
+    console.log(id);
+    
     if (updatedCom) {
         const response = await fetch(`/api/comment/${id}`, {
             method: 'PUT',
@@ -20,6 +20,28 @@ const updateCommentBtn = async (event) => {
     }
 };
 
+const update = (event) => {
+    event.preventDefault();
+    const updateText = document.createElement('input');
+    const updateButton = document.createElement('button');
+    const commentCard = document.querySelector('.comment');
+    const updateBtn = document.querySelector('#updateBtn');
+    const id = event.target.getAttribute('data-commentId');
+    
+
+    updateText.type = 'text';
+    updateText.id = 'updateComment';
+    updateButton.type = 'button';
+    updateButton.textContent = 'Save';
+    updateButton.setAttribute('data-commentId', id);
+    updateButton.addEventListener('click', updateCommentBtn);
+    commentCard.appendChild(updateText);
+    commentCard.appendChild(updateButton)
+
+    // commentCard.removeChild(updateBtn);
+    
+};
+
 if (document.querySelector('.updateBtn')) {
-document.querySelector('.updateBtn').addEventListener('click', updateCommentBtn);
+document.querySelector('.updateBtn').addEventListener('click', update);
 };
